0a69d22c7d99b95cfb0a3a3fead7fc31610a7386
DidaPradana <42725223+didapradana@users.noreply.github.com>
2018-09-10 11:26:48 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,36 @@
 <?php
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+$angka = 5;
+function kesatu($x, $y){
+	if ($x > 0) {
+		if ($y > 0) {
+			echo "* ";
+			$y--;
+			kesatu($x, $y);
+		}
+		else{
+			echo "<br>";
+			$x--;
+			kesatu($x, $x);
+		}
+	}
 }
-rekursif(5);
+kesatu ($angka, $angka);
+echo "=========================================================<br>";
+// NomerDua
+function kedua($x, $y, $angka){
+	if ($x <= $angka) {
+		if ($y <= $x) {
+			echo "$y ";
+			$y++;
+			kedua($x, $y, $angka);
+		}
+		else{
+			echo "<br>";
+			$x++;
+			kedua($x, 1, $angka);
+		}
+	}
+}
+kedua (1, 1, $angka);
 ?>
